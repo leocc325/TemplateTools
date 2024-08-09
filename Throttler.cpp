18c33cf1c2b8692b6525f4087b0ef5b08d38d44f
leocc325 <56433061+leocc325@users.noreply.github.com>
2024-08-09 09:52:13 +0800

    Update Throttler.cpp

--- Throttler.cpp
+++ Throttler.cpp
@@ -1,3 +1,15 @@
 #include "Throttler.hpp"
 
 std::unordered_map<std::size_t,AbstractThrottle*> Throttler::funcMap;
+std::mutex Throttler::mapMutex;
+
+ThrottleError::ThrottleError(const char *info):errorString(info){}
+
+ThrottleError::ThrottleError(const std::string &info):errorString(info){}
+
+ThrottleError::ThrottleError(std::string &&info):errorString(std::move(info)){}
+
+const char *ThrottleError::what() const noexcept
+{
+    return errorString.data();
+}
