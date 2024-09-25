43011b844e4e331e5a328a54161fe33f42679e85
leocc325 <56433061+leocc325@users.noreply.github.com>
2024-09-25 17:06:28 +0800

    Update ThrottlePrivate.hpp

--- ThrottlePrivate.hpp
+++ ThrottlePrivate.hpp
@@ -16,21 +16,6 @@
 
 #include "FunctionTraits.hpp"
 
-///判断返回值是否是void
-template<typename Func>
-struct ReturnVoid
-{
-    using RT =typename MetaUtility::FunctionTraits<typename std::decay<Func>::type>::ReturnType;
-    constexpr static bool value = std::is_same<RT,void>::value;
-};
-
-///判断Func是否是自由函数或成员函数指针
-template<typename Func>
-struct IsFunctionPointer{
-    using FreeFunc = typename std::remove_pointer<Func>::type;
-    constexpr static bool value  = std::is_function<FreeFunc>::value || std::is_member_function_pointer<Func>::value;
-};
-
 ///每一个函数都有一个与之对应的Throttle对象专门管理这个函数的调用
 class AbstractThrottle : public QObject
 {
@@ -39,13 +24,13 @@ public:
 
     virtual ~AbstractThrottle();
 
+    void addTask(std::function<void()>&& task);
+
 protected:
     bool isEmpty();
 
     ///线程函数,用于处理任务队列,调用每次有任务进入队列时都尝试启动这个线程函数,队列中任务为空时结束线程
     void taskThread();
-
-    void addTask(std::function<void()>&& task);
 
     virtual void processTask() = 0;
 
@@ -57,29 +42,12 @@ protected:
     std::chrono::steady_clock::time_point m_LastCall;
 };
 
-///同步节流
+///同步节流,当SyncThrottle在多个线程中addTask时,最终会将任务转发至创建SyncThrottle对象时所在的线程
+///因为事件循环的线程是在SyncThrottle对象被创建时确定的
 class SyncThrottle:public AbstractThrottle
 {
 public:
     SyncThrottle(std::size_t interval);
-
-    template<typename Func,typename...Args>
-    typename std::enable_if<IsFunctionPointer<Func>::value && ReturnVoid<Func>::value>::type
-    call(Func func,Args&&...args)
-    {
-        m_TargetThread =  this->thread();
-        std::function<void()> task = std::bind(std::forward<Func>(func),std::forward<Args>(args)...);
-        this->addTask(std::move(task));
-    }
-
-    template<typename Func,typename Obj,typename...Args>
-    typename std::enable_if<IsFunctionPointer<Func>::value && ReturnVoid<Func>::value>::type
-    call(Func func,Obj* obj,Args&&...args)
-    {
-        m_TargetThread = this->thread();
-        std::function<void()> task = std::bind(std::forward<Func>(func),obj,std::forward<Args>(args)...);
-        this->addTask(std::move(task));
-    }
 
 protected:
     void processTask() override;
@@ -93,22 +61,6 @@ class AsyncThrottle:public AbstractThrottle
 {
 public:
     AsyncThrottle(std::size_t interval);
-
-    template<typename Func,typename...Args>
-    typename std::enable_if<IsFunctionPointer<Func>::value && ReturnVoid<Func>::value>::type
-    call(Func func,Args&&...args)
-    {
-        std::function<void()> task = std::bind(std::forward<Func>(func),std::forward<Args>(args)...);
-        this->addTask(std::move(task));
-    }
-
-    template<typename Func,typename Obj,typename...Args>
-    typename std::enable_if<IsFunctionPointer<Func>::value && ReturnVoid<Func>::value>::type
-    call(Func func,Obj* obj,Args&&...args)
-    {
-        std::function<void()> task = std::bind(std::forward<Func>(func),obj,std::forward<Args>(args)...);
-        this->addTask(std::move(task));
-    }
 
 protected:
     void processTask() override;
