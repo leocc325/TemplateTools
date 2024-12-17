4f4aa35a7470e41b8d7bd60f58f5bacbfa3885ae
leocc325 <56433061+leocc325@users.noreply.github.com>
2024-12-17 14:16:45 +0800

    Update ThrottlePrivate.hpp

--- ThrottlePrivate.hpp
+++ ThrottlePrivate.hpp
@@ -6,61 +6,56 @@
 #include <chrono>
 #include <mutex>
 #include <thread>
+#include <condition_variable>
 #include <utility>
 #include <list>
 #include <sstream>
 
 #include <QApplication>
 #include <QThread>
-#include <QDebug>
 
 ///每一个函数都有一个与之对应的Throttle对象专门管理这个函数的调用
 class AbstractThrottle : public QObject
 {
-public:
-    AbstractThrottle(std::size_t interval);
-
-    virtual ~AbstractThrottle();
-
-    void addTask(std::function<void()>&& task);
-
+    friend class Throttler;
+    
 protected:
-    bool isEmpty();
-
-    ///线程函数,用于处理任务队列,调用每次有任务进入队列时都尝试启动这个线程函数,队列中任务为空时结束线程
-    void taskThread();
-
+    AbstractThrottle(std::size_t interval);
+    virtual ~AbstractThrottle();
     virtual void processTask() = 0;
 
+private:
+    void addTask(std::function<void()>&& task);
+    bool isEmpty();
+    void taskThread();
+
 protected:
+    std::condition_variable m_CV;
     std::mutex m_Mutex;
-    std::atomic<bool> m_IsRunning{false};
     std::list<std::function<void()>> m_TaskQue;
     std::chrono::milliseconds m_Interval{1000};
     std::chrono::steady_clock::time_point m_LastCall;
+    std::atomic<bool> m_QuitFlag;
 };
 
 ///同步节流,当SyncThrottle在多个线程中addTask时,最终会将任务转发至创建SyncThrottle对象时所在的线程
 ///因为事件循环的线程是在SyncThrottle对象被创建时确定的
 class SyncThrottle:public AbstractThrottle
 {
-public:
+     friend class Throttler;
+private:
     SyncThrottle(std::size_t interval);
-
-protected:
     void processTask() override;
-
-protected:
+private:
     QThread* m_TargetThread = nullptr;
 };
 
 ///异步节流
 class AsyncThrottle:public AbstractThrottle
 {
-public:
+    friend class Throttler;
+private:
     AsyncThrottle(std::size_t interval);
-
-protected:
     void processTask() override;
 };
 
