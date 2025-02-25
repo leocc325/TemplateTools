b55f04f6b2289947078faacbc8d437bbc33acf14
leocc325 <56433061+leocc325@users.noreply.github.com>
2025-02-25 23:24:09 +0800

    Update ParameterSerializer.hpp

--- ParameterSerializer.hpp
+++ ParameterSerializer.hpp
@@ -1,42 +1,77 @@
 #ifndef PARAMETERSERIALIZER_HPP
 #define PARAMETERSERIALIZER_HPP
 
-//template<unsigned...Bitss>
+#include <string.h>
+#include <malloc.h>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+template <unsigned...Bytes>
 struct ParameterSerializer
 {
-    template<unsigned Bit,unsigned...Bits>
+private:
+    template<unsigned Byte,unsigned...RemainBytes>
     struct Length{
-        static constexpr unsigned value = Bit + Length<Bits...>::value;
+        static constexpr unsigned value = Byte + Length<RemainBytes...>::value;
     };
 
-    template<unsigned Bit>
-    struct Length<Bit>{
-        static constexpr unsigned value = Bit;
+    template<unsigned Byte>
+    struct Length<Byte>{
+        static constexpr unsigned value = Byte;
     };
 
-    template<unsigned Bit,unsigned...Bits>
-    static constexpr unsigned len()
+    template<typename First,typename...Args>
+    struct IsInteger{
+        static constexpr bool value = std::is_integral<First>::value && IsInteger<Args...>::value;
+    };
+
+    template<typename First>
+    struct IsInteger<First>{
+        static constexpr bool value = std::is_integral<First>::value;
+    };
+
+    template<unsigned ArgNum,unsigned BytesNum,typename...Args>
+    struct Matchable{
+        static constexpr bool value = (ArgNum==BytesNum) && IsInteger<Args...>::value;
+    };
+
+    template<unsigned Byte,unsigned...RemainBytes>
+    struct TransImpl{
+        template<typename First,typename...Args>
+        static unsigned char* trans(unsigned char* data,unsigned pos,First first,Args...args)
+        {
+            unsigned char* ch= static_cast<unsigned char*>(alloca(Byte));
+            ch = reinterpret_cast<unsigned char*>(first);
+            for(unsigned i = 0; i < Byte; i++){
+                data[pos+i] = ch[i];
+            }
+            return TransImpl<RemainBytes...>::trans(data,pos+Byte,std::forward<Args>(args)...);
+        }
+    };
+
+    template<unsigned Byte>
+    struct TransImpl<Byte>{
+        template<typename Arg>
+        static unsigned char* trans(unsigned char* data,unsigned pos,Arg arg)
+        {
+            unsigned char* ch= static_cast<unsigned char*>(alloca(Byte));
+            ch = reinterpret_cast<unsigned char*>(arg);
+            for(int unsigned i = 0; i < Byte; i++){
+                data[pos+i] = ch[i];
+            }
+            return data;
+        }
+    };
+public:
+    template<typename...Args>
+    static typename std::enable_if<Matchable<sizeof... (Args),sizeof... (Bytes),Args...>::value,unsigned char*>::type
+    trans(Args...args)
     {
-        return Bit + len<Bits...>();
+        unsigned char* data = new unsigned char[Length<Bytes...>::value];
+        return TransImpl<Bytes...>::trans(data,0,std::forward<Args>(args)...);
     }
 
-    template<>
-    static constexpr unsigned len<unsigned Bit>()
-    {
-        return Bit;
-    }
-
-//    template<typename...Args>
-//    static unsigned char* trans(Args...args)
-//    {
-//        unsigned char* data = new unsigned char[Length<Bitss...>::value];
-//    }
-
-//    template<unsigned Pos,unsigned...Bit,typename First,typename...Args>
-//    static unsigned char* transImpl(unsigned char* data,const First& first,const Args&...args)
-//    {
-
-//    }
 };
 
 #endif // PARAMETERSERIALIZER_HPP
