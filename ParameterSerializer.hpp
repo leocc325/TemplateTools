e18d880d204bb194ad3484267521ee5b3701f4a6
leocc325 <56433061+leocc325@users.noreply.github.com>
2025-02-27 10:29:48 +0800

    Update ParameterSerializer.hpp

--- ParameterSerializer.hpp
+++ ParameterSerializer.hpp
@@ -62,12 +62,11 @@ struct IsInteger<First>{
     static constexpr bool value = std::is_integral<First>::value;
 };
 
-template<unsigned ArgNum,unsigned BytesNum,typename...Args>
+///类模板参数数量和函数模板参数数量一致，而且每一个实参都是整数
+template<unsigned ClassArgNum,unsigned FuncArgNum,typename...Args>
 struct Matchable{
-    static constexpr bool value = (ArgNum==BytesNum) && IsInteger<Args...>::value;
+    static constexpr bool value = (ClassArgNum==FuncArgNum) && IsInteger<Args...>::value;
 };
-
-
 
 template <unsigned...Bytes>
 class ParameterSerializer
@@ -126,7 +125,7 @@ class ParameterSerializer
 
 public:
     template<typename...Args>
-    static typename std::enable_if<Matchable<sizeof... (Args),sizeof... (Bytes),Args...>::value,unsigned char*>::type
+    static typename std::enable_if<Matchable<sizeof... (Bytes),sizeof... (Args),Args...>::value,unsigned char*>::type
     transToBigEndian(Args...args)
     {
         unsigned char* data = new unsigned char[Length<Bytes...>::value];
@@ -134,7 +133,7 @@ public:
     }
 
     template<typename...Args>
-    static typename std::enable_if<Matchable<sizeof... (Args),sizeof... (Bytes),Args...>::value,unsigned char*>::type
+    static typename std::enable_if<Matchable<sizeof... (Bytes),sizeof... (Args),Args...>::value,unsigned char*>::type
     transToLittleEndian(Args...args)
     {
         unsigned char* data = new unsigned char[Length<Bytes...>::value];
@@ -148,31 +147,63 @@ public:
 
 enum ByteMode {BigEndian,LittleEndian};
 
-template<ByteMode Mode>
+template<ByteMode Mode = BigEndian>
 class FrameSerializer
 {
 public:
     template<unsigned...Bytes>
     class FixLengthFrame {
+    private:
+        ///类模板参数数量
+        static constexpr unsigned ClassArgSize = sizeof... (Bytes);
 
-        static constexpr bool SingleBytes = (sizeof... (Bytes) == 1);
+        ///帧总长度
+        static constexpr unsigned FrameLength = Length<Bytes...>::value;
 
-        template<unsigned Byte,unsigned...RemainBytes> struct TransImpl
+        ///如果表示每一个数据所占字节大小的模板参数数量为1，而且字节长度总数等于实际参数数量，则认为这是符合情况1的
+        template<unsigned ClassArgNum,unsigned BytesLength,typename...Args>
+        struct OneBytePerArg{
+            static constexpr bool value = (ClassArgNum==1) && (BytesLength == sizeof... (Args)) && IsInteger<Args...>::value;
+        };
+
+        ///情况1:数据帧长度固定为N,每一个数据占用1字节的长度时调用此递归模板
+        template<unsigned Byte> struct FixTransSingle
         {
-            template<typename First,typename...Args> static unsigned char* trans(unsigned char* data,unsigned pos,First first,Args...args)
+            template<typename First,typename...Args>
+            static unsigned char* transImpl(unsigned char* data,unsigned pos,First first,Args...args)
+            {
+                data[pos] = static_cast<unsigned char>(first) & 0xFF ;
+                return FixTransSingle<Byte>::transImpl(data,pos+1,std::forward<Args>(args)...);
+            }
+
+            template<typename Arg>
+            static unsigned char* transImpl(unsigned char* data,unsigned pos,Arg arg)
+            {
+                data[pos] = static_cast<unsigned char>(arg) & 0xFF ;
+                return data;
+            }
+        };
+
+        ///情况2:数据帧长度固定为N,每一个数据占用的字节长度不等时调用此递归模板FixTrans
+        template<unsigned Byte,unsigned...RemainBytes> struct FixTrans
+        {
+            template<typename First,typename...Args>
+            static unsigned char* transImpl(unsigned char* data,unsigned pos,First first,Args...args)
             {
                 for(unsigned i = 0; i < Byte; i++)
                 {
                     unsigned offset = (Mode == BigEndian) ? (i * __CHAR_BIT__ ) : (Byte - i - 1) * __CHAR_BIT__;
                     data[pos+i] = static_cast<unsigned char>(first >> offset) & 0xFF ;
                 }
-                return TransImpl<RemainBytes...>::trans(data,pos+Byte,std::forward<Args>(args)...);
+                return FixTrans<RemainBytes...>::transImpl(data,pos+Byte,std::forward<Args>(args)...);
             }
         };
 
-        template<unsigned Byte> struct TransImpl<Byte>
+        ///情况2:数据帧长度固定为N,每一个数据占用的字节长度不等时的递归模板终止条件
+        template<unsigned Byte> struct FixTrans<Byte>
         {
-            template<typename Arg> static unsigned char* trans(unsigned char* data,unsigned pos,Arg arg)
+            template<typename Arg>
+            static unsigned char* transImpl(unsigned char* data,unsigned pos,Arg arg)
             {
                 for(int unsigned i = 0; i < Byte; i++)
                 {
@@ -183,12 +214,30 @@ public:
             }
         };
 
+
     public:
+        ///总长度固定,单个数据长度可变的转换
         template<typename...Args>
-        static unsigned char* transToBigEndian(Args...args)
+        static typename std::enable_if<!OneBytePerArg<ClassArgSize,FrameLength,Args...>::value,unsigned char*>::type
+        trans(Args...args)
         {
-            unsigned char* data = new unsigned char[Length<Bytes...>::value];
-            return TransImpl<Bytes...>::bigEndian(data,0,std::forward<Args>(args)...);
+            ///判断参数数量、字节长度、和模板参数长度是否一致
+            static_assert (Matchable<ClassArgSize,sizeof... (Args),Args...>::value,"error");
+
+            unsigned char* data = new unsigned char[FrameLength];
+            return FixTrans<Bytes...>::transImpl(data,0,std::forward<Args>(args)...);
+        }
+
+        ///总长度固定,单个数据长度1字节的转换
+        template<typename...Args>
+        static typename std::enable_if<OneBytePerArg<ClassArgSize,FrameLength,Args...>::value,unsigned char*>::type
+        trans(Args...args)
+        {
+            ///判断参数数量和模板指定的帧长度是否一致
+            static_assert (sizeof... (Args) == FrameLength,"error");
+
+            unsigned char* data = new unsigned char[FrameLength];
+            return FixTransSingle<Bytes...>::transImpl(data,0,std::forward<Args>(args)...);
         }
 
     };
@@ -201,7 +250,8 @@ private:
 };
 
 void test(){
-    FrameSerializer<BigEndian>::FixLengthFrame<1,2,3>::
+    FrameSerializer<BigEndian>::FixLengthFrame<4>::trans(4,5,6,7);
+    FrameSerializer<BigEndian>::FixLengthFrame<1,1,2,3>::trans(4,5,6,7);
 }
 
 #endif // PARAMETERSERIALIZER_HPP
