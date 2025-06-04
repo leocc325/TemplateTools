edba39ab11efc096f28b86ecdf3bd0bd9d7c7608
leocc325 <56433061+leocc325@users.noreply.github.com>
2025-06-04 15:55:07 +0800

    Update UnitConvertor.hpp

--- UnitConvertor.hpp
+++ UnitConvertor.hpp
@@ -70,7 +70,7 @@ namespace UnitConvertor
 
         template<typename R>
         struct Unit{
-            static constexpr char unit = '?';
+            const char* unit = " ?";
         };
 
         template<>
@@ -99,6 +99,16 @@ namespace UnitConvertor
     using Peta   =   std::peta;
     using Exa    =   std::exa;
 
+    enum ExpEnum{ExpNum};
+
+    enum UnitEnum{UnitNum};
+
+    struct ValuePack{
+      double value;
+      ExpEnum exp;
+      UnitEnum unit;
+    };
+
     ///将以From为单位的数值转换为以To为单位表示的数值
     template<typename From,typename To>
     static typename std::enable_if<IsRatio<From>::value&&IsRatio<To>::value,double>::type
@@ -111,38 +121,45 @@ namespace UnitConvertor
 
     ///将以From为单位的数值转换为以To为单位表示的字符串
     template<typename From,typename To>
-    static typename std::enable_if<IsRatio<From>::value&&IsRatio<To>::value,double>::type
+    static typename std::enable_if<IsRatio<From>::value&&IsRatio<To>::value,QString>::type
     toString(double value)
     {
-
+        using RatioType = typename std::ratio_divide<From,To>::type;
+        double newValue = toValue<From,To>(value);
+        return QString("%1 %2%3").arg(QString::number(newValue)).arg(2).arg(3);
     }
 
     ///将数值自动转换为一个恰当单位表示的数值(1～999之间的值),函数内部递归
     ///如果递归导致ratio溢出或者参数已经是一个恰当的值则返回当前的值和ratio
     template<typename From>
-    static double toProperValue(double value)
+    static typename std::enable_if<IsRatio<From>::value,ValuePack>::type
+    toProperValue(double value)
     {
         if(value > 1000)//如果数据大于1000则将其除以1000并上调数量级
             return toProperValue<std::ratio_multiply<From,Kilo>>(value/1000);
         else if(value < 1)//如果数据小于1则将其乘以1000并上调数量级
             return toProperValue<std::ratio_divide<From,Kilo>>(value*1000);
         else//如果数据刚好在[1,1000)区间内,则认为这已经是一个恰当的值
-            return value;
+            return ValuePack{.value = value , .exp = ExpNum, .unit = UnitNum};
     }
 
-    ///模板本身会无限地归导致std::ratio分子/分母溢出,所以这里需要特化一个模板专门处理溢出的情况
-    template<> double toProperValue<std::atto>(double value)
-    { return value; }
+    ///模板本身会无限地归导致std::ratio分子/分母溢出,所以这里需要特化模板专门处理溢出的情况
+    template<> ValuePack toProperValue<std::atto>(double value)
+    {
+        return ValuePack{.value = value , .exp = ExpNum, .unit = UnitNum};
+    }
 
-    template<> double toProperValue<std::exa>(double value)
-    { return value; }
+    template<> ValuePack toProperValue<std::exa>(double value)
+    {
+        return ValuePack{.value = value , .exp = ExpNum, .unit = UnitNum};
+    }
 
     ///将数值自动转换为一个恰当单位表示的字符串
     template<typename From>
-    static typename std::enable_if<IsRatio<From>::value,double>::type
+    static typename std::enable_if<IsRatio<From>::value,QString>::type
     toProperString(double value)
     {
-        using RatioType = typename std::ratio_divide<From,One>::type;
+        ValuePack pack = toProperValue<From>(value);
     }
 };
 
