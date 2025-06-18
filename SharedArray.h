42420f81c80be8ef4379447b21f684c8ed6905c4
leocc325 <56433061+leocc325@users.noreply.github.com>
2025-06-18 18:16:45 +0800

    Update SharedArray.h

--- SharedArray.h
+++ SharedArray.h
@@ -4,11 +4,41 @@
 #include <QDebug>
 #include <string.h>
 #include <memory>
+#include <vector>
 
 template <typename T>
 class SharedArray
 {
 public:
+    // stl compatibility
+    typedef size_t size_type;
+    typedef const T& const_reference;
+    typedef T& reference;
+    typedef T* pointer;
+    typedef const T* const_pointer;
+    typedef T value_type;
+
+    typedef T* iterator;
+    typedef const T* const_iterator;
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+    inline iterator begin();
+    inline const_iterator begin() const;
+    inline const_iterator cbegin() const;
+    inline const_iterator constBegin() const;
+    inline iterator end();
+    inline const_iterator end() const;
+    inline const_iterator cend() const;
+    inline const_iterator constEnd() const;
+    reverse_iterator rbegin() { return reverse_iterator(end()); }
+    reverse_iterator rend() { return reverse_iterator(begin()); }
+    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+    const_reverse_iterator crbegin() const { return const_reverse_iterator(end()); }
+    const_reverse_iterator crend() const { return const_reverse_iterator(begin()); }
+
+
     SharedArray(){}
 
     SharedArray(T* array,std::size_t length) noexcept
@@ -42,7 +72,7 @@ public:
         size = other.size;
     }
 
-    operator const T* () const noexcept
+    inline operator const T* () const noexcept
     {
         //这个函数不需要detach,因为返回的是指针的拷贝而不是指针的引用
         //外部直接修改指针地址不会影响data所指向的地址
@@ -56,15 +86,11 @@ public:
 
     T& operator[] (std::size_t index)
     {
-        if(data.use_count() > 1)
-            detach();
-
         return (data.get()[index]);
     }
 
     const T& operator[] (std::size_t index) const
     {
-        qDebug()<<"const []";
         return (data.get()[index]);
     }
 
@@ -72,12 +98,15 @@ private:
     void detach()
     {
         //detach无需判空,对空指针的写操作会直接导致段错误,所以如果能写就必定能拷贝
-        qDebug()<<"detach";
-        std::shared_ptr<T> newData = std::shared_ptr<T>(new T[size],std::default_delete<T[]>());
-        memcpy(newData.get(),data.get(),sizeof (T) * size);
-        data = newData;
+        if(data.use_count() > 1)
+        {
+            std::shared_ptr<T> newData = std::shared_ptr<T>(new T[size],std::default_delete<T[]>());
+            memcpy(newData.get(),data.get(),sizeof (T) * size);
+            data = newData;
+        }
     }
-    public:
+
+private:
     std::shared_ptr<T> data = nullptr;
     size_t size = 0;
 };
