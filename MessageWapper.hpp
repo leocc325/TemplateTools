413c13abb2e2e17724a4e02315aa171148c6a030
leocc325 <56433061+leocc325@users.noreply.github.com>
2025-07-05 01:06:23 +0800

    Update MessageWapper.hpp

--- MessageWapper.hpp
+++ MessageWapper.hpp
@@ -10,12 +10,6 @@
 #include <QDebug>
 #include <QStringList>
 
-#define USE_MEMPOOL 1
-
-#if USE_MEMPOOL
-#include "TemplateTools/MemoryPool.hpp"
-#endif
-
 using namespace MetaUtility;
 
 //使用消息id获取返回值类型
@@ -50,8 +44,11 @@ class MessageWapper
         static void setMsgArgs(void* from,void* to)
         {
             //需要在外面确保两个指针的类型一致,否则会UB
-            resetTuple(to);
-            *static_cast<ArgsTuple*>(to) = *static_cast<ArgsTuple*>(from);
+            if(from == nullptr)
+                return;
+
+            initTuple(to);
+            *static_cast<ArgsTuple*>(to) =  *static_cast<ArgsTuple*>(from);
         }
 
         static void setStringArgs(const QStringList& argsList,void* argsTuple)
@@ -61,7 +58,7 @@ class MessageWapper
                 return;
             }
 
-            resetTuple(argsTuple);
+            initTuple(argsTuple);
             TupleHelper<Arity - 1, ArgsTuple>::set(*static_cast<ArgsTuple*>(argsTuple), argsList);
         }
 
@@ -70,6 +67,7 @@ class MessageWapper
             deleter(static_cast<ArgsTuple*>(args));
             deleter(static_cast<Ret*>(ret));
         }
+
         //有内存池的情况下可以将这里改为内存池分配空间
         static void initMembers(void*& args,const std::type_info*& argsInfo,void*& ret,const std::type_info*& retInfo)
         {
@@ -82,12 +80,8 @@ class MessageWapper
         {
             if(ptr != nullptr)
             {
-#if USE_MEMPOOL
-                MemoryPool::GlobalPool->deallocate(ptr);
-#else
                 ptr->~T();
                 ::operator delete(ptr);
-#endif
                 ptr = nullptr;
             }
         }
@@ -97,11 +91,7 @@ class MessageWapper
         {
             if(ptr != nullptr)
             {
-#if USE_MEMPOOL
-                MemoryPool::GlobalPool->deallocate(ptr);
-#else
                 ::operator delete(ptr);
-#endif
                 ptr = nullptr;
             }
         }
@@ -111,11 +101,7 @@ class MessageWapper
         {
             if(ptr == nullptr)
             {
-#if USE_MEMPOOL
-                ptr = MemoryPool::GlobalPool->allocate<T>();
-#else
                 ptr = static_cast<T*>(::operator new(sizeof (T)));
-#endif
                 info = &typeid(T);
             }
         }
@@ -144,15 +130,12 @@ class MessageWapper
 
         }
 
-        static void resetTuple(void*& tpl)
+        //考虑这里是否不需要使用placement new
+        static void initTuple(void*& tpl)
         {
             if(tpl == nullptr)
             {
-#if USE_MEMPOOL
-                tpl = MemoryPool::GlobalPool->allocate<ArgsTuple>();
-#else
                 tpl = ::operator new(sizeof (ArgsTuple));
-#endif
             }
             else
             {
@@ -169,7 +152,6 @@ public:
     MessageWapper(const MessageWapper& other)
     {
         this->functor = other.functor;
-        this->initHelper = other.initHelper;
         this->msgArgsHelper = other.msgArgsHelper;
         this->scpiArgsHelper = other.scpiArgsHelper;
         this->deleteHelper = other.deleteHelper;
@@ -177,15 +159,15 @@ public:
         this->resultString = other.resultString;
 
         //指针拷贝完成之后对参数指针初始化
-        this->initHelper(argsTuple,argTupleInfo,result,resultInfo);
+        this->resultInfo = other.resultInfo;
+        this->argTupleInfo = other.argTupleInfo;
+        this->msgArgsHelper(other.argsTuple,this->argsTuple);
+        this->resultHelper(other.result,this->result);
     }
 
     MessageWapper(MessageWapper&& other) noexcept
     {
         this->functor = std::move(other.functor);
-
-        this->initHelper = other.initHelper;
-        other.initHelper = nullptr;
 
         this->msgArgsHelper = other.msgArgsHelper;
         other.msgArgsHelper = nullptr;
@@ -228,10 +210,14 @@ public:
         msgArgsHelper = &Manager<Ret,ArgsTuple>::setMsgArgs;
         scpiArgsHelper = &Manager<Ret,ArgsTuple>::setStringArgs;
         deleteHelper = &Manager<Ret,ArgsTuple>::deleteMembers;
-        initHelper = &Manager<Ret,ArgsTuple>::initMembers;
         resultHelper = &Manager<Ret,ArgsTuple>::result;
 
-        initHelper(argsTuple,argTupleInfo,result,resultInfo);
+        //构造函数不对保存参数和结果的指针分配内存,仅仅在需要往这两个指针中写入数据时才初始化,避免对为设置参数的MessageWapper调用exec()引起UB
+        //这两个指针要么为空表示没有保存数据,要么不为空表示已经设置好数据
+        result = nullptr;
+        argsTuple = nullptr;
+        resultInfo = &typeid (Ret);
+        argTupleInfo = &typeid(ArgsTuple);
 
         //这里不能将this捕获到lambda中,当lambda捕获类的成员变量时,它实际上捕获的是this指针。
         //拷贝对象时,lambda中的this指针仍然指向原对象A导致新对象B执行函数时错误地访问 A 的数据。
@@ -239,23 +225,25 @@ public:
         //B和C中成员变量无论如何变化,functor的执行结果都和A的执行结果一样,因为functor持有的MessageWapper*是A
         //所以需要将其更改成调用时传入MessageWapper指针
         functor = [func,obj](MessageWapper* ptr){
+            if(ptr->argsTuple == nullptr)
+            {
+                qDebug()<<"no parameter has been setted,excute failed";
+                return;
+            }
+
             ptr->callHelper(func,obj);
         };
     }
 
-    ~MessageWapper()
+    ~MessageWapper() = delete;
+
+    void free()
     {
         deleteHelper(argsTuple,result);
     }
 
     void exec()
     {
-        if(argsTuple == nullptr)
-        {
-            qDebug()<<"no parameter has been setted,excute failed";
-            return;
-        }
-
         functor(this);
     }
 
@@ -295,10 +283,18 @@ public:
     {
         if(typeid(RT) != *resultInfo)
         {
+            //如果类型不匹配说明代码参数错误,直接报异常
             throw std::invalid_argument("return value type mismatch");
         }
 
-        return *static_cast<RT*>(result);
+        if(result == nullptr)
+        {
+            //如果是业务流程导致没有计算结果则返回一个对应的零值,同时打印错误信息
+            qWarning()<<"incorrect return value due to nullptr result";
+            return RT{};
+        }
+        else
+            return *static_cast<RT*>(result);
     }
 
     template<std::size_t Index,typename RT = typename MessageReturnType<Index>::type>
@@ -406,7 +402,6 @@ private:
 
     void (*msgArgsHelper)(void*,void*) = nullptr;
     void (*scpiArgsHelper)(const QStringList&,void*) = nullptr;
-    void (*initHelper)(void*&,const std::type_info*&,void*&,const std::type_info*&) = nullptr;
     void (*deleteHelper)(void*,void*) = nullptr;
     void (*resultHelper)(void*,void*) = nullptr;
 
@@ -478,54 +473,8 @@ inline TestClass* testFunc2(int a,TestClass*)
 
 inline void testUnit()
 {
-    TestClass* t = MemoryPool::GlobalPool->allocate<TestClass>();
-    MemoryPool::GlobalPool->deallocate(t);
-
-    TestClass* obj = new TestClass();
-
     //测试MessageWapper构造函数的默认参数
-    //MessageWapper msg_001(testFunc,obj);//类型推导错误，编译器报错
-    //MessageWapper msg_002(&TestClass::func1,obj);//类型推导正确,编译通过
-    //MessageWapper msg_003(&TestClass::func1);//类型推导正确,编译通过
 
-    MessageWapper msg_004A(testFunc);
-    //msg_004A.callByMsg(10,20);//传入错误数量的参数,抛出异常
-    //msg_004A.callByMsg(10,20.5,30.5);//传入类型错误的参数,抛出异常
-    msg_004A.callByMsg(10,20,20.5);//传入正确的参数,输出testFunc:  10 20 20.5
-
-    {
-        MessageWapper msgA(testFunc);
-        MessageWapper msgB = msgA;
-
-        msgA.setMsgArgs(1,2,3.0);
-        msgB.setMsgArgs(10,20,20.0);
-
-        msgA.exec();
-        msgB.exec();
-        qDebug()<<"ssss";
-    }
-
-
-    int result = 0;
-    msg_004A.getResult(&result);
-    qDebug()<<result;
-
-    msg_004A.callByMsg(10,20,30.0);
-    msg_004A.getResult(&result);
-
-    {
-      MessageWapper ms(testFunc2);
-    }
-    {
-      MessageWapper ms(testFunc2);
-    }
-    MessageWapper msg_005(testFunc2);
-
-
-    msg_005.callByMsg(10,obj);
-    msg_005.callByMsg(10,obj);
-    msg_005.callByMsg(10,obj);
-    msg_005.callByMsg(10,obj);
 
     //测试返回值为void和非void的函数
     //测试普通函数、成员函数、static函数、lambda、std::function、callable
