563df5c1c7dc49675dcff36b8845e9aecd00faa8
leocc325 <56433061+leocc325@users.noreply.github.com>
2025-07-09 11:35:49 +0800

    Update MetaEnum.hpp

--- MetaEnum.hpp
+++ MetaEnum.hpp
@@ -1,109 +1,135 @@
-﻿#ifndef METAENUM_HPP
+#ifndef METAENUM_HPP
 #define METAENUM_HPP
 
 #include <QString>
 #include <vector>
 #include <QPair>
 #include <set>
+#include <QDebug>
+
+/**
+ * @EnumMapBase:建议在EnumMapBase及其子类的任何对象时都使用static创建,避免重复调用其构造函数
+ * 如果有特别的效率要求可以将映射容器从std::vector更改为QHash
+*/
+template<typename T>
+struct EnumMapBase
+{
+    EnumMapBase(){}
+
+    EnumMapBase(const std::vector<std::pair<T,QString>>& vec):map(vec){}
+
+    T toEnum(const QString& str)
+    {
+        auto it = map.cbegin();
+        while (it != map.cend())
+        {
+            if(it->second == str)
+            {
+                return T(it->first);
+            }
+            ++it;
+        }
+        qWarning()<<"error:The corresponding enum value for the string was not found. "
+                    "Please check the input string or the enum mapping table.";
+        return T{};
+    }
+
+    QString toString(T value)
+    {
+        auto it = map.cbegin();
+        while (it != map.cend())
+        {
+            if(it->first == value)
+            {
+                return it->second;
+            }
+            ++it;
+        }
+        qWarning()<<"The corresponding string for the enum value was not found. "
+                    "Please check the input enum or the enum mapping table.";
+        return QString();
+    }
+
+    const std::vector<std::pair<T,QString>> map = {};
+};
 
 template<typename T>
-struct EnumMap
+struct EnumMap : EnumMapBase<T>
 {
+    //用于判断时普通枚举还是注册了可以和自定义字符串转换的枚举
     constexpr static bool registed = false;
-    const std::vector<std::pair<T,QString>> map= {};
-    //***如果没有在map中找到需要打印提示信息,有可能是字符串输入错误，也有可能是map中录入的键值对缺失或错误
 };
 
 template <typename Key,typename Value = QString,typename...Args>
-static std::vector<std::pair<Key,Value>> generateEnumMap(Key key,Value value,Args...args)
+static std::vector<std::pair<Key,Value>>
+generateEnumMap(const Key& key,const Value& value,Args...args)
 {
+    qDebug()<<__PRETTY_FUNCTION__;
     static_assert(sizeof...(Args) % 2 == 0, "Arguments must come in pairs: enum value followed by QString");
-    return std::vector<std::pair<Key,Value>>{};
+
+    const unsigned vecSize = (sizeof...(Args) / 2) + 1;
+    std::vector<std::pair<Key,Value>> vec;
+    vec.reserve(vecSize);
+    generateImpl(vec,key,value,std::forward<Args>(args)...);
+    return vec;
 }
 
-// 注册枚举的宏
+template <typename Key,typename Value = QString,typename...Args>
+static std::vector<std::pair<Key,Value>>
+generateImpl(std::vector<std::pair<Key,Value>>& vec,const Key& key,const Value& value,Args...args)
+{
+    vec.emplace_back(key,value);
+    generateImpl(vec,std::forward<Args>(args)...);
+    return vec;
+}
+
+template <typename Key,typename Value = QString>
+static std::vector<std::pair<Key,Value>>
+generateImpl(std::vector<std::pair<Key,Value>>& vec,const Key& key,const Value& value)
+{
+    vec.emplace_back(key,value);
+    return vec;
+}
+
+/**
+ * @RegMetaEnum:注册枚举的宏
+ */
+
+#if 0 /**第一版宏**/
 #define RegMetaEnum(EnumType, ...)    \
 template <>     \
 struct EnumMap<EnumType> \
 {    \
 constexpr static bool registed = true;  \
-const std::vector<std::pair<EnumType,QString>> map= generateEnumMap(__VA_ARGS__) ;\
+const std::vector<std::pair<EnumType,QString>> map = generateEnumMap(__VA_ARGS__) ;\
 };
+#else /**第二版宏**/
+#define RegMetaEnum(EnumType, ...)    \
+template<>  \
+struct EnumMap<EnumType>:EnumMapBase<EnumType>{ \
+EnumMap(): EnumMapBase<EnumType>(generateEnumMap(__VA_ARGS__)){}    \
+constexpr static bool registed = true;  \
+};
+#endif
 
+/**示例**/
 enum Color{Red,Blue = 5,Green};
-RegMetaEnum(Color,Red,QString("Red"),Blue,QString("Blue"),Green,QString("Green"))
-
-//template<>
-//struct EnumMap<Color>{
-//    constexpr static bool registed = true;
-//    const std::vector<std::pair<Color,QString>> map= {
-//        std::pair<Color,QString>{Red,"Red"},
-//        std::pair<Color,QString>{Blue,"Blue"},
-//        std::pair<Color,QString>{Green,"Green"}
-//    };
-//};
-
-enum Fruit{Apple,Banana,Orange};
-RegMetaEnum(Fruit,Apple,QString("Apple"),Banana,QString("Banana"),Orange,QString("Orange"))
-
-//template<>
-//struct EnumMap<Fruit>{
-//    constexpr static bool registed = true;
-//    const std::vector<std::pair<Fruit,QString>> map= {
-//        std::pair<Fruit,QString>{Apple,"Apple"},
-//        std::pair<Fruit,QString>{Banana,"Banana"},
-//        std::pair<Fruit,QString>{Orange,"Orange"}
-//    };
-//};
-
-enum Math{Add,Divide,FFT};
-
-template<typename T,typename std::enable_if<EnumMap<T>::registed>::type>
-QString enumToString(T value)
-{
-    /**这里使用迭代器区查找就不要计算键值对的数量了**/
-    EnumMap<T> emap;
-    auto it = emap.map.cbegin();
-    while (it != emap.map.cend())
-    {
-        if(it->first == value)
-        {
-            return it->second;
-        }
-        ++it;
-    }
-
-    return QString();
-}
-
-template<typename T,typename std::enable_if<!EnumMap<T>::registed>::type>
-QString enumToString(T value)
-{
-    return QString::number(value);
-}
-
-template<typename T>
-typename std::enable_if<EnumMap<T>::registed,T>::type
-stringToEnum(const QString& str,T& arg)
-{
-    EnumMap<T> emap;
-    auto it = emap.map.cbegin();
-    while (it != emap.map.cend())
-    {
-        if(it->second == str)
-        {
-            arg = T(it->first);
-        }
-        ++it;
-    }
-}
-
-template<typename T>
-typename std::enable_if<!EnumMap<T>::registed,T>::type
-stringToEnum(const QString& str,T& arg)
-{
-    arg = T(str.toInt());
-}
-
+#if 1
+    RegMetaEnum(Color,
+                Red,QString("Red"),
+                Blue,QString("Blue"),
+                Green,QString("Green"))
+#else
+static const std::vector<std::pair<Color,QString>> colorVec= {
+    std::pair<Color,QString>{Red,"Red"},
+    std::pair<Color,QString>{Blue,"Blue"},
+    std::pair<Color,QString>{Green,"Green"}
+};
+template<>
+struct EnumMap<Color>:EnumMapBase<Color>{
+    EnumMap(): EnumMapBase<Color>(colorVec) {}
+    constexpr static bool registed = true;
+};
+#endif
 
 #endif // METAENUM_HPP
